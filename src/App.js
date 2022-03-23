524a4d5978f4f19c113290df0ccdf96b2ebf77b8
p <oxidenexw@gmail.com>
2022-03-23 16:10:52 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+// Import ABI Code to interact with smart contract
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+import "./App.css";
+
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+
+
+function App() {
+  // Property Variables
+
+  const [message, setMessage] = useState("");
+  const [currentGreeting, setCurrentGreeting] = useState("");
+
+  // Helper Functions
+
+  // Requests access to the user's Meta Mask Account
+  // https://metamask.io/
+  async function requestAccount() {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  }
+
+  // Fetches the current value store in greeting
+  async function fetchGreeting() {
+    // If MetaMask exists
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        greeterAddress,
+        Greeter.abi,
+        provider
+      );
+      try {
+        // Call Greeter.greet() and display current greeting in `console`
+        /* 
+          function greet() public view returns (string memory) {
+            return greeting;
+          }
+        */
+        const data = await contract.greet();
+        console.log("data: ", data);
+        setCurrentGreeting(data);
+      } catch (error) {
+        console.log("Error: ", error);
+      }
+    }
+  }
+
+  // Sets the greeting from input text box
+  async function setGreeting() {
+    if (!message) return;
+
+    // If MetaMask exists
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      // Create contract with signer
+      /*
+        function setGreeting(string memory _greeting) public {
+          console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+          greeting = _greeting;
+        } 
+      */
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(message);
+
+      setMessage("");
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
+
+  // Return
+  return (
+    <div className="App">
+      <div className="App-header">
+        {/* DESCRIPTION  */}
+        <div className="description">
+          <h1>ARATOS</h1>
+          <h3>FETCH AND SET IMPLEMANTATION</h3>
+        </div>
+        {/* BUTTONS - Fetch and Set */}
+        <div className="custom-buttons">
+          <button onClick={fetchGreeting} style={{ backgroundColor: "green" }}>
+            Fetch Message
+          </button>
+          <button onClick={setGreeting} style={{ backgroundColor: "red" }}>
+            Set Message
+          </button>
+        </div>
+        {/* INPUT TEXT - String  */}
+        <input
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          placeholder="Set Message"
+        />
+
+        {/* Current Value stored on Blockchain */}
+        <h2 className="greeting">Message: {currentGreeting}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
